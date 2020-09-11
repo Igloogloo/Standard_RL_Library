f2d0b9b926f8d6e80f6305f4415dcbaaace6c6ca
Igloogloo <55815140+Igloogloo@users.noreply.github.com>
2020-09-11 19:51:26 -0400

    Oracle that gives feedback
    Oracle class. Currently has a perfect binary oracle that will always give correct binary feedback (if the action taken is optimal: 1, if the action taken is not optimal: 0), as well as a binary oracle for policy shaping (takes 'likelihood of feedback' as well 'probability that feedback is optimal' parameters). (Yet to be added is a TAMER oracle).
    
    This class assumes the q-table the oracle uses is in pandas DataFrame format (conversion from numpy arrays is fairly straight forward).

---
+++ Oracle.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import random
+
+
+# The following is a general purpose Oracle used to simulate human feedback for reinforcement learning tasks.
+
+class Oracle:
+    def __init__(self, qtable=None):  # Takes in a qtable in pandas dataframe format. (Can easily convert from an
+        # np array)
+        # This implementation assumes states are represented by columns and actions by rows.
+        self.qtable = qtable
+
+    # The following is a simple oracle which returns feedback of 1 if the action is optimal and -1 otherwise
+    def get_binary_feedback(self, state, action):
+        optimal_action = self.qtable.loc[state].idxmax()
+        if self.qtable.loc[state][optimal_action] == self.qtable.loc[state][action]:
+            return 1
+        else:
+            return -1
+
+    # The following function simulates a human by, given a state and an action already taken , returning binary feedback
+    # The ps stands for policy shaping.
+    # The parameter liklihd stands for likelihood and is the probability that the oracle will provide feedback.
+    # The parameter conf is the confidence that the oracle's feedback is optimal.
+    def get_binary_feedback_ps(self, state, action, liklihd, conf):
+        if liklihd < 0 or liklihd > 1:
+            raise ValueError('Likelihood parameter must be a value between 0 and 1')
+
+        if np.random.random() > liklihd:  # If True, 0 feedback is returned
+            return 0
+
+        if conf < 0 or conf > 1:
+            raise ValueError('Confidence parameter must be a value between 0 and 1')
+
+        if conf > np.random.random():  # If True, oracle will provide feedback optimally 
+            optimal_action = self.qtable.loc[state].idxmax()
+            if self.qtable.loc[state][optimal_action] == self.qtable.loc[state][action]:
+                return 1
+            else:
+                return -1
+        else:
+            optimal_action = self.qtable.loc[state].idxmax()
+            if self.qtable.loc[state][optimal_action] == self.qtable.loc[state][action]:
+                return -1
+            else:
+                return 1
