7cf1da3c3b8754c37bd0501475594c511610dbd6
Igloogloo <55815140+Igloogloo@users.noreply.github.com>
2020-10-02 13:09:30 -0400

    Added TorchOracle for Pytorch applications

--- Oracle.py
+++ Oracle.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import random
-
+import torch
+import torch.nn as nn
 
 # The following is a general purpose Oracle used to simulate human feedback for reinforcement learning tasks.
 
@@ -55,8 +56,7 @@ class NPOracle:
         # This implementation assumes states are represented by columns and actions by rows.
         self.qtable = qtable
 
-        # The following is a simple oracle which returns feedback of 1 if the action is optimal and -1 otherwise
-
+    # The following is a simple oracle which returns feedback of 1 if the action is optimal and -1 otherwise
     def get_binary_feedback(self, state, action):
         optimal_action = np.argmax(self.qtable[state])
         if self.qtable[state][optimal_action] == self.qtable[state][action]:
@@ -64,11 +64,10 @@ class NPOracle:
         else:
             return -1
 
-        # The following function simulates a human by, given a state and an action already taken , returning binary feedback
-        # The ps stands for policy shaping.
-        # The parameter liklihd stands for likelihood and is the probability that the oracle will provide feedback.
-        # The parameter conf is the confidence that the oracle's feedback is optimal.
-
+    # The following function simulates a human by, given a state and an action already taken , returning binary feedback
+    # The ps stands for policy shaping.
+    # The parameter liklihd stands for likelihood and is the probability that the oracle will provide feedback.
+    # The parameter conf is the confidence that the oracle's feedback is optimal.
     def get_binary_feedback_ps(self, state, action, liklihd, conf):
         if liklihd < 0 or liklihd > 1:
             raise ValueError('Likelihood parameter must be a value between 0 and 1')
@@ -95,3 +94,55 @@ class NPOracle:
 
     def get_qval(self, state, action):
         return self.qtable[state][action]
+
+    
+class TorchOracle:
+    # Pytorch oravle class. Create an oracle with a pretrained model.
+    # The "state" parameter is the input the oracle will evaluate. The action
+    # parameter is the action that was taken by the agent that the oracle 
+    # is evaluating against. 
+    # All parameters are assumed to be pytorch tensors.
+    def __init__(self, model):
+        self.model = model
+        model.eval()
+    
+    # The following is a simple oracle which returns feedback of 1 if the action is optimal and -1 otherwise
+    def get_binary_feedback(self, state, action):
+        with torch.no_grad():
+            output = self.model(state)
+            optimal_action = torch.argmax(output)
+            optimal_action = optimal_action.item()
+            if optimal_action == action.item():
+                return 1
+            else: 
+                return -1
+    
+    # The following function simulates a human by, given a state and an action already taken , returning binary feedback
+    # The ps stands for policy shaping.
+    # The parameter liklihd stands for likelihood and is the probability that the oracle will provide feedback.
+    # The parameter conf is the confidence that the oracle's feedback is optimal.
+    def get_binary_feedback_ps(self, state, action, liklihd, conf):
+        if liklihd < 0 or liklihd > 1:
+            raise ValueError('Likelihood parameter must be a value between 0 and 1')
+
+        if np.random.random() > liklihd:  # If True, 0 feedback is returned
+            return 0
+
+        if conf < 0 or conf > 1:
+            raise ValueError('Confidence parameter must be a value between 0 and 1')
+        
+        with torch.no_grad():
+            output = self.model(state)
+            optimal_action = torch.argmax(output)
+            optimal_action = optimal_action.item()
+            print(f'optimal: {optimal_action}, action: {action.item()}')
+            if conf > np.random.random():  # If True, oracle will provide feedback optimally
+                if optimal_action == action.item():
+                    return 1
+                else: 
+                    return -1
+            else: 
+                if optimal_action == action.item():
+                    return -1
+                else:
+                    return 1
